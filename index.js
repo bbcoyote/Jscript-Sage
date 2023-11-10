83249d31d41d185269577c0ec5e7042369e952c2
Allen Huffman <huffmancharles14@gmail.com>
2023-11-10 04:49:28 -0600

    add global variables for card buttons, fixed portfolio button ID

---
+++ index.js
@@ -0,0 +1,15 @@
+//Top Card buttons
+const accountButton = document.getElementById("profile-settings");
+const previewAccount = document.getElementById("profile-preview");
+const profileSearch = document.getElementById("profile-search");
+const profileAnalytics = document.getElementById("profile-analytics");
+
+//middle card buttons
+const contactInfo = document.getElementById("contact-info");
+const socialLinks = document.getElementById("social-links");
+const skillView = document.getElementById("skills-view");
+const portfolioView = document.getElementById("portfolio-view");
+
+//Bottom Card Buttons
+const messageButton = document.getElementById("message");
+const followButton = document.getElementById("follow");
